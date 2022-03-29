13b86e025df64ea0c10afa60555e57c542c8e92b
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-03-29 02:10:25 +0000

    done

--- main.cs
+++ main.cs
@@ -1,7 +1,24 @@
+// Created by: Jaejun Lee
+// Created on: Mar 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function calculates the area and perimeter of a triangle
+        float hours;
+        float hourlyrate;
+      
+        Console.WriteLine("Enter number of hours worked:");
+        hours = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Enter hourly rate:");
+        hourlyrate = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Your pay will be: $" + ((hours * hourlyrate) * (1 - 0.18)).ToString("0.00") + ".");
+        Console.WriteLine("The government will take: $" + ((hours * hourlyrate) * 0.18).ToString("0.00") + ".");
+        Console.WriteLine("\nDone.");
+    }
 }
